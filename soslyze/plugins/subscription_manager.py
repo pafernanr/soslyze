5648bb2c0acb10f0632365202e3de8f15d163204
Pablo Fernández Rodríguez <pafernan@redhat.com>
2025-04-29 20:43:15 +0200

    remove subscription-manager dependency
    Signed-off-by: Pablo Fernández Rodríguez <pafernan@redhat.com>

--- soslyze/plugins/subscription_manager.py
+++ soslyze/plugins/subscription_manager.py
@@ -1,16 +1,21 @@
-from io import StringIO
-from pathlib import Path
-from contextlib import redirect_stdout
-from rct.cli import CatCertCommand  # noqa F401
-import re
+import base64
+import json
 import os
+from pathlib import Path
+import re
+import zlib
 
-from soslyze.utils import print_headline, print_value, parse_text
+from soslyze.utils import parse_text
+from soslyze.utils import print_headline
+from soslyze.utils import print_value
 
 
 class SubscriptionManager:
     def __init__(self, path):
         lines = []
+        ent_start = '-----BEGIN ENTITLEMENT DATA-----'
+        ent_end = '-----END ENTITLEMENT DATA-----'
+        # "1.3.6.1.4.1.2312.9"
         if os.path.isfile(path + '/etc/rhsm/rhsm.conf'):
             self.platform = parse_text(
                 path + '/etc/rhsm/rhsm.conf',
@@ -21,14 +26,16 @@ class SubscriptionManager:
         self.content_access = False
 
         if os.path.isdir(path + '/etc/pki/entitlement/'):
-            for cert in os.listdir(path + '/etc/pki/entitlement/'):
-                catcher = StringIO()
-                with redirect_stdout(catcher):
-                    exec('CatCertCommand().main(args=["' + path +
-                         '/etc/pki/entitlement/' + cert +
-                         '", "--no-products", "--no-content"])')
-                for line in catcher.getvalue().splitlines():
-                    if re.search(r'.*SKU:.*', line):
+            for certname in os.listdir(path + '/etc/pki/entitlement/'):
+                with open(f"{path}/etc/pki/entitlement/{certname}",
+                          encoding="utf-8") as f:
+                    pem = re.findall(
+                        f'{ent_start}(.*?){ent_end}', f.read(), re.DOTALL)
+                    b64 = base64.b64decode(pem[0])
+                    decompressed = zlib.decompress(b64).decode('utf-8')
+                    j = json.loads(decompressed)
+                    # dict_keys(['consumer', 'subscription', 'order', 'products', 'pool'])
+                    if "subscription" in j and "sku" in j['subscription']:
                         self.content_access = True
                         break
         if os.path.isfile(
@@ -41,8 +48,8 @@ class SubscriptionManager:
                 "SKU|Start|Ends|Pool ID|Status Details).*")
         if os.path.isdir(path + '/etc/rhsm/facts/'):
             for fact in os.listdir(path + '/etc/rhsm/facts/'):
-                lines.append(Path(
-                    path + '/etc/rhsm/facts/' + fact).read_text())
+                lines.append(Path(f"{path}/etc/rhsm/facts/{fact}")
+                             .read_text(encoding="utf-8"))
             self.facts = '\n'.join(lines)
             lines.clear()
         if os.path.isfile(
@@ -50,7 +57,7 @@ class SubscriptionManager:
                 "subscription-manager_identity"):
             self.lfce = Path(
                 path + "/sos_commands/subscription_manager/" +
-                "subscription-manager_identity").read_text()
+                "subscription-manager_identity").read_text(encoding="utf-8")
         if os.path.isfile(path + '/dmidecode'):
             parse_text(path + '/dmidecode', r".*uuid.*", options=re.IGNORECASE)
 
